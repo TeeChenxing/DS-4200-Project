f197cf24a7d6311ed1c73b114917bce373e7b4d4
Souren33 <prakashsouren@gmail.com>
2024-11-09 22:19:08 -0500

    'sankey'

---
+++ code/sankey.py
@@ -0,0 +1,80 @@
+"""
+File: SP_sankey.py
+Author: Souren Prakash
+
+Description: A wrapper library for plotly Sankey visualizations
+"""
+
+import pandas as pd
+import plotly.graph_objects as go
+
+
+class SPSankey:
+    def __init__(self, df, val_col, **kwargs):
+        """
+        Initialize the Sankey class with a dataframe, value column, and other column names
+        """
+        self.df = df
+        self.val_col = val_col
+        self.columns = list(kwargs.values())
+
+    def code_mapping(self):
+        """
+        Create a mapping of unique values in 'src' and 'targ' to integer codes
+        """
+        sources = {}
+        i = 0
+        for name in pd.concat([self.df['src'], self.df['targ']]).unique():
+            sources[name] = i
+            i += 1
+        self.df = self.df.replace({'src': sources, 'targ': sources})
+        self.labels = list(sources.keys())
+        return self.df, self.labels
+
+    def stacking(self):
+        """
+        Stack the dataframe by creating 'src' and 'targ' columns for the Sankey diagram
+        """
+        stacked_data = pd.DataFrame()
+        for i in range(len(self.columns) - 1):
+            src_col = self.columns[i]
+            targ_col = self.columns[i + 1]
+            temp_df = self.df[[src_col, targ_col, self.val_col]]
+            temp_df = temp_df.rename(columns={src_col: 'src', targ_col: 'targ'})
+            temp_df = temp_df.groupby(['src', 'targ'])[self.val_col].sum().reset_index()
+            stacked_data = pd.concat([stacked_data, temp_df], ignore_index=True)
+        stacked_data = stacked_data.dropna()
+        self.df = stacked_data
+        return self.df
+
+    def make_sankey(self):
+        """
+        Create and display the Sankey diagram
+        """
+        self.stacking()
+        self.code_mapping()
+
+        values = self.df[self.val_col] if self.val_col else [1] * len(self.df)
+        link = {'source': self.df['src'], 'target': self.df['targ'], 'value': values}
+        node = {'label': self.labels}
+
+        sk = go.Sankey(link=link, node=node)
+        fig = go.Figure(sk)
+        fig.show()
+
+
+def main():
+    data = {'A': ['u', 'u', 'u', 'v'],
+            'B': ['x', 'y', 'y', 'x'],
+            'C': ['p', 'p', 'q', 'p'],
+            'Values': [10, 20, 15, 15]}
+
+    df = pd.DataFrame(data)
+
+    # Initialize Sankey class and generate Sankey diagram
+    sankey_obj = SPSankey(df, 'Values', col1='A', col2='B', col3='C')
+    sankey_obj.make_sankey()
+
+
+if __name__ == '__main__':
+    main()
